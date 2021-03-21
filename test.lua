4f72d9fe274f644672f269fdc7941ebfc2a858ab
869646063@qq.com <869646063@qq.com>
2021-03-21 18:44:13 +0800

    更是实现

--- test.lua
+++ test.lua
@@ -1,32 +1,47 @@
--- key->value的空表
+require "utils"
+
+
 local t = {
-  a = {},
-  b = {},
-  c = {},
+  -- a = {
+  --   a1 = {
+  --     a2 = {
+  --       a3 = {
+  --       }
+  --     }
+  --   }
+  -- },
+  -- b = {
+  --   b1 = {
+  --     b2 = {
+  --       b3 = {
+  --       }
+  --     }
+  --   }
+  -- },
+  -- c = { "我", "是", "谁" },
+  -- d = { { 1, 2, 3 }, { 4, 5, 6} },
+  -- e = { a = 1, b = 2, c = 3 },
+  -- f = {true, false, null},
+  -- key = "key", value = "value",
+  -- f = function () end,
 }
 
--- 嵌套数组
-local t = {
-  query = {
-    { a = { 1, 2, 3 } },
-    { b = { 4, 5, 6 } },
-    { c = { 7, 8, 9 } },
-  }
-}
 
--- 多层嵌套数组
-local t = {
-  query = {
-    { { { 1 }, { 2 }, { 3 } } },
-    { { 4, 5, 6 } },
-    { { 7, 8, 9 } },
-  }
-}
-
+-- 使用 lxml
 local lxml = require "lxml"
+-- 编码
 local xml = lxml.encode(t)
 print(xml)
+-- 解码
+local tab = lxml.decode(xml)
+var_dump(tab)
 
-local xml2lua = require "xml2lua"
-local xml = xml2lua.toxml(t, "xml")
-print(xml)
+
+-- -- 使用 xml2lua
+-- local xml2lua = require "xml2lua"
+-- -- 编码
+-- local xml = xml2lua.toxml(t, "xml")
+-- print(xml)
+-- -- 解码
+-- local xml = xml2lua.parser(xml)
+-- var_dump(xml)
